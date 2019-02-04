5dab52346fe980d20af3e61c75b36e2f791f4c08
Chris Drackett <chris@drackett.com>
2019-02-04 11:24:56 -0800

    adding react-native-web

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,12 @@
 // @flow
 
 import * as React from "react";
+import { Text, View } from "react-native";
 
 import core from "@fm/core";
 
-export default () => <div>Test Website! {core}</div>;
+export default () => (
+  <View>
+    <Text>Test Website! {core}</Text>
+  </View>
+);
